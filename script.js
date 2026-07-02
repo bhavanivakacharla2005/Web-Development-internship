600813ac2c93abb95fc0c75a62c503a4c9ff842b
bhavanivakacharla2005 <bhavanivakacharla2005@gmail.com>
2026-07-02 11:21:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,75 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+let cart = [];
+let total = 0;
 
-anchor.addEventListener("click", function(e){
+function addToCart(name, price){
 
-e.preventDefault();
+    cart.push({
+        name,
+        price
+    });
 
-document.querySelector(
-this.getAttribute("href")
-).scrollIntoView({
-behavior:"smooth"
-});
+    total += price;
 
-});
+    updateCart();
+}
+
+function updateCart(){
+
+    const cartItems =
+        document.getElementById("cart-items");
+
+    cartItems.innerHTML = "";
+
+    cart.forEach(item => {
+
+        const li =
+            document.createElement("li");
+
+        li.textContent =
+            `${item.name} - ₹${item.price}`;
+
+        cartItems.appendChild(li);
+    });
+
+    document.getElementById("cart-count")
+        .textContent = cart.length;
+
+    document.getElementById("total")
+        .textContent = total;
+}
+
+function clearCart(){
+
+    cart = [];
+    total = 0;
+
+    updateCart();
+}
+
+document
+.getElementById("search")
+.addEventListener("keyup", function(){
+
+    let value =
+        this.value.toLowerCase();
+
+    let products =
+        document.querySelectorAll(".product");
+
+    products.forEach(product => {
+
+        let name =
+        product.querySelector("h3")
+        .textContent
+        .toLowerCase();
+
+        if(name.includes(value)){
+            product.style.display = "block";
+        }
+        else{
+            product.style.display = "none";
+        }
+
+    });
 
 });
